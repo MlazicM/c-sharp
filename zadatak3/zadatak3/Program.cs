9f21e71de897d6e1c4eb63f9dfa91ec530f4ee47
MlazicM <miloslazic458@gmail.com>
2026-09-03 20:29:02 +0200

    Add files via upload

---
+++ zadatak3/zadatak3/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Unesite prvi broj : ");
+int a  = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Unesite drugi broj : ");
+int b = Convert.ToInt32(Console.ReadLine());
+bool prvijeveci = a > b;
+bool drugijeveci = b > a;
+bool jednaki = a == b;
+
+if(prvijeveci)
+{
+    Console.WriteLine("Prvi broj je veci od drugog broja");
+}
+else if (drugijeveci)
+{
+    Console.WriteLine("Drugi broj je veci od prvog broja");
+}
+else if (jednaki)
+{
+    Console.WriteLine("Brojevi su jednaki");
+}
---
+++ zadatak4/zadatak4/Program.cs
@@ -0,0 +1,10 @@
+﻿int a = 10;
+if(a > 0)
+{
+    Console.WriteLine("a je pozitivan");
+}
+for(int i = 0; i < 5; i++)
+{
+    int kvadrat = i * i;
+    Console.WriteLine("Kvadrat broja {0} je {1}", i, kvadrat);
+}
